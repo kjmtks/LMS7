a3d2bc20a60ebdc3db29967f017085bc1363bb1f
Takehsi Kojima <kojima@tokushima-u.ac.jp>
2020-03-15 23:21:42 +0900

    Implement temporary for file stream response

---
+++ ALMS.App/Controllers/RawFileController.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Linq;
+using ALMS.App.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ALMS.App.Controllers
+{
+    public class RawFileController : Controller
+    {
+        DatabaseService DB;
+        public RawFileController(DatabaseService db)
+        {
+            DB = db;
+        }
+
+        [HttpGet("/lecture/{owner_account}/{lecture_name}/contents/{branch}/{**path}")]
+        public IActionResult LectureContentsFileRaw(string owner_account, string lecture_name, string branch, string path)
+        {
+            var lecture = DB.Context.Lectures.Include(x => x.Owner).Include(x => x.LectureUsers).Where(x => x.Owner.Account == owner_account && x.Name == lecture_name).FirstOrDefault();
+
+            if (lecture == null) return new NotFoundResult();
+
+            var ms = lecture.LectureContentsRepositoryPair.SharedRepository.ReadFileWithoutTypeCheck(Path.Combine("pages", path), branch);
+
+            var provider = new FileExtensionContentTypeProvider();
+            if (!provider.TryGetContentType(path, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            return File(ms, contentType);
+            
+        }
+    }
+}
