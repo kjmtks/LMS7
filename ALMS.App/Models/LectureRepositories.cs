45a88a5e09673ea9b4683ebced4b56a0c34ddc39
Takeshi Kojima <kojima@medsci.tokushima-u.ac.jp>
2020-03-30 14:49:46 +0900

    Bug fix: Git API URL

--- ALMS.App/Models/LectureRepositories.cs
+++ ALMS.App/Models/LectureRepositories.cs
@@ -27,7 +27,7 @@ namespace ALMS.App.Models
 
         public override LectureContentsClonedRepository ClonedRepository => lectureContentsClonedRepository;
 
-        public override string ApiUrl => $"/api/git/lecture/{lecture.Owner.Account}/{lecture.Name}.contents.git";
+        public override string ApiUrl => $"{Environment.GetEnvironmentVariable("APP_URL")}/api/git/lecture/{lecture.Owner.Account}/{lecture.Name}.contents.git";
 
         public override bool CanPull(User user)
         {
@@ -84,7 +84,7 @@ namespace ALMS.App.Models
 
         public override LectureSubmissionsClonedRepository ClonedRepository => lectureSubmissionsClonedRepository;
 
-        public override string ApiUrl => $"/api/git/lecture/{lecture.Owner.Account}/{lecture.Name}.submissions.git";
+        public override string ApiUrl => $"{Environment.GetEnvironmentVariable("APP_URL")}/api/git/lecture/{lecture.Owner.Account}/{lecture.Name}.submissions.git";
 
         public override bool CanPull(User user)
         {
